2f417129d77562a19a3473cdeff3548df74f8503
Alex <olexiyskok@gmail.com>
2024-09-24 20:50:49 +0300

    convert miles to km

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,26 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static double miles = 0.621371192;
 
     public static void main(String[] args){
         System.out.println("Convertor distance app");
+        Scanner inputKM = new Scanner(System.in);
+        System.out.println("Введіть відстань в кілометрах: ");
+        int km = inputKM.nextInt();
+        System.out.println("Ви ввели " + km + " ки " + " - в мілях це буде " + convertKmtoMiles(km) + " мілі");
+        Scanner inputMiles = new Scanner(System.in);
+        System.out.println("Введіть відстань в милях: ");
+        int mile = inputMiles.nextInt();
+        System.out.println("Ви ввели " + mile + " милі " + " - в кілмоетрах це буде " + convertMilesToKm(mile) + " км");
+    }
+    private static double convertKmtoMiles(int km){
+        return km * miles;
+    }
+    private static double convertMilesToKm(int mile){
+        return mile/miles;
     }
 }
